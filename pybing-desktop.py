b05e61debed509af2ec84ecafd84abf1a7cabc12
aizquierdo <aizquierdo@mrmilu.com>
2019-08-27 10:46:05 +0200

    some mods

---
+++ pybing-desktop.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+"""
+Python script to set bing image of the day as desktop wallpaper
+OS: Ubuntu 16.04
+Author: Anurag Rana
+More Info: https://www.pythoncircle.com
+"""
+
+import datetime
+import json
+import os
+
+import requests
+
+IMAGE_PATH = "/home/alex/Imágenes/Wallpapers/bing/"
+
+# get image url
+response = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
+image_data = json.loads(response.text)
+
+image_url = image_data["images"][0]["url"]
+image_url = image_url.split("&")[0]
+full_image_url = "https://www.bing.com" + image_url
+
+# image's name
+image_name = datetime.date.today().strftime("%Y%m%d")
+image_extension = image_url.split(".")[-1]
+image_name = image_name + "." + image_extension
+image_path = IMAGE_PATH + image_name
+
+# download and save image
+img_data = requests.get(full_image_url).content
+with open(image_path, 'wb') as handler:
+    handler.write(img_data)
+
+# ubuntu command to set wallpaper
+os.system("`which gsettings` set org.gnome.desktop.background picture-uri file://" + image_path)
