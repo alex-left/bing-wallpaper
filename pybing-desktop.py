b60de856958c4ef543f47f3c40666b0d22c983e8
aizquierdo <aizquierdo@mrmilu.com>
2019-12-05 10:57:06 +0100

    updated

--- pybing-desktop.py
+++ pybing-desktop.py
@@ -9,37 +9,41 @@ Forked from: Anurag Rana
 import datetime
 import json
 import subprocess
-
 import requests
+import time
 
 IMAGE_PATH = "/home/alex/Imágenes/Wallpapers/bing/"
 
 # get image url
 base_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
-response = requests.get(base_url)
-image_data = json.loads(response.text)
+interval = 3000
 
-image_url = image_data["images"][0]["url"]
-image_url = image_url.split("&")[0]
-full_image_url = "https://www.bing.com" + image_url
+while True:
+    response = requests.get(base_url)
+    image_data = json.loads(response.text)
 
-# image's name
-image_name = datetime.date.today().strftime("%Y%m%d")
-image_extension = image_url.split(".")[-1]
-image_name = image_name + "." + image_extension
-image_path = IMAGE_PATH + image_name
+    image_url = image_data["images"][0]["url"]
+    image_url = image_url.split("&")[0]
+    full_image_url = "https://www.bing.com" + image_url
 
-# download and save image
-img_data = requests.get(full_image_url).content
-with open(image_path, 'wb') as handler:
-    handler.write(img_data)
+    # image's name
+    image_name = datetime.date.today().strftime("%Y%m%d")
+    image_extension = image_url.split(".")[-1]
+    image_name = image_name + "." + image_extension
+    image_path = IMAGE_PATH + image_name
 
-try:
-    a = subprocess.check_call(["/usr/bin/gsettings",
-                               "set",
-                               "org.gnome.desktop.background",
-                               "picture-uri",
-                               "file://" + image_path])
-    print(a)
-except subprocess.CalledProcessError as e:
-    print(e)
+    # download and save image
+    img_data = requests.get(full_image_url).content
+    with open(image_path, 'wb') as handler:
+        handler.write(img_data)
+
+    try:
+        a = subprocess.run(["/usr/bin/gsettings",
+                            "set",
+                            "org.gnome.desktop.background",
+                            "picture-uri",
+                            "file://" + image_path], check=True)
+        # print(a)
+    except subprocess.CalledProcessError as e:
+        print(e)
+    time.sleep(interval)
